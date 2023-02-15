3539a2da1d8b2cc9bca02c7b79905fa7cbe2fae8
lawlessrd <r.dylan.lawless@vumc.org>
2023-02-15 10:03:12 -0500

    Added function to remove duplicate entries and entries > 100 chars in length for Recap sync module

--- examcard2txt/convert_to_csv.py
+++ examcard2txt/convert_to_csv.py
@@ -13,7 +13,7 @@ import glob
 import numpy as np
 import csv
 import pandas
-
+import collections
 
 def search_string_in_file(file_name, string_to_search, starting_line):
     """
@@ -89,16 +89,27 @@ def main(argv):
 
     # Scan txt file for scan names, extract parameters
     scan_dict={}
+    tmp=[]
     search_string=[]
     for scan in scan_list:
         scan_dict[scan] = []
         string_to_search = 'Protocol Name:  ' + scan
         scan_start = search_string_in_file(examcard_txt,string_to_search,0)[0][0]
+        scan_end = search_string_in_file(examcard_txt,'Protocol Name:  ',int(scan_start)+1)
+        if not scan_end:
+            scan_end = search_string_in_file(examcard_txt,'Converted by examcard2txt',0)
+
         for para_options in para_dict.keys():
             for i in range(len(para_dict[para_options])):
                 para = para_dict[para_options][i]
+
                 tmp=search_string
+
                 search_string = search_string_in_file(examcard_txt,para,scan_start)
+                # verify that value is within scan range in examcard
+                if search_string and search_string[0][0] > scan_end[0][0]:
+                    search_string = []
+                # verify that we are pulling unique values
                 if search_string and tmp != search_string:
                     scan_start = search_string[0][0]
                     scan_dict[scan].append(search_string[0][1])
@@ -106,14 +117,34 @@ def main(argv):
                     scan_start = search_string[0][0]+1
                     search_string = search_string_in_file(examcard_txt,para,scan_start)
                     scan_dict[scan].append(search_string[0][1])
-                else:
-                    continue
+                elif not search_string:
+                    # Insert NOT FOUND if parameter is not in examcard
+                    scan_dict[scan].append(para + ': NOT FOUND')
 
-    # Add parameters to csv
-    # One scan per row, first column is scan name
-    with open(examcard_csv, "w") as csv_file:
-        for key in scan_dict.keys():
-            csv_file.write("%s,%s\n"%(key,scan_dict[key]))
+    # Adjust dict for redcap sync module
+    # Append spaces to duplicate entries and remove entries > 100 characters in length
+    uniq = {}
+
+    for key in scan_dict.keys():
+        seen = set()
+        tmp = []
+        uniq[key] = []
+        for val in scan_dict[key]:
+            if val not in seen and len(val) < 100:            
+                seen.add(val)
+                uniq[key].append(val)
+            elif len(val) < 100:
+                uniq[key].append(val + ' ')
+                seen.add(val + ' ')
+            else:
+                uniq[key].append(val[:99])
+                seen.add(val[:99])
+
+        # Add parameters to csv
+        # One scan per row, first column is scan name
+        with open(examcard_csv, "w") as csv_file:
+            for key in uniq.keys():
+                csv_file.write("%s,%s\n"%(key,uniq[key]))
 
 
 if __name__ == '__main__':
